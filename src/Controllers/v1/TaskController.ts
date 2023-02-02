e6b3fb495b48e3dfc131371a6e7aeb6e79b85446
Awais Ahmed <ahmed@cefriel.it>
2023-02-02 20:42:02 +0100

    fix

--- src/Controllers/v1/TaskController.ts
+++ src/Controllers/v1/TaskController.ts
@@ -11,20 +11,24 @@ class TaskController {
     const page = Number(request.query.page)
     const limit = Number(request.query.limit)
     const offset = (page - 1) * limit
-    //const result = await RoleService.getRole(request.body.employee_id)
-    //posso anche semplicemente filtrare poi il risultato in modo che il viewer vede i sui risultati
-    //if (result.dataValues.role_id === 1 || result.dataValues.role_id === 2) {
+    const viewertask = []
     const tasks = await TaskService.all({ offset, limit })
-    response.json(tasks)
-    /*} else {
-      response.status(401).send({ message: 'Unauthorized' })
-    }*/
+    if (process.env.DB_USER === 'admin' || process.env.DB_USER === 'editor') {
+      response.json(tasks)
+    } else {
+      for (const task in tasks) {
+        if (tasks[task].dataValues.employee_id === 3) {
+          viewertask.push(tasks[task])
+        }
+      }
+      response.json(viewertask)
+    }
   }
 
   public async store(request: Request, response: Response) {
     const payload = request.body
     const result = await RoleService.getRole(request.body.employee_id)
-    if (result.dataValues.role_id === 1) {
+    if (result.dataValues.name === 'admin') {
       const task = await TaskService.store(payload)
       response.json({ data: task })
     } else {
@@ -47,26 +51,21 @@ class TaskController {
   public async update(request: Request, response: Response) {
     const { id } = request.params
     const payload = request.body
-    const result = await EmployeeService.getRoleForUpdate(request.body.employee_id)
-    switch (result.dataValues.role_id) {
-      case 1:
+    switch (process.env.DB_USER) {
+      case 'admin':
+        //admin può assegnare anche task
         const taskAdmin = await TaskService.update(id, payload)
         response.json({ data: taskAdmin })
         break
-      case 2:
+      case 'editor':
         //editor può editare solo lo stato
-        const taskEditor = await TaskService.update(id, payload.status)
+        const status = { status: payload.status }
+        const taskEditor = await TaskService.update(id, status)
         response.json({ data: taskEditor })
         break
       default:
         response.status(401).send({ message: 'Unauthorized' })
         break
-    }
-    if (result.dataValues.role_id === 1 || result.dataValues.role_id === 2) {
-      const task = await TaskService.update(id, payload)
-      response.json({ data: task })
-    } else {
-      response.status(401).send({ message: 'Unauthorized' })
     }
   }
 
